ca3748011a0f952d733ffe19f7b8311d9a48c2fa
Colin White <clwhite814@gmail.com>
2019-05-05 17:35:29 -0500

    getting all seed data leapers

---
+++ QuantumLeap/Models/Leaper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuantumLeap.Models
+{
+    public class Leaper
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public decimal Budget { get; set; }
+    }
+}
